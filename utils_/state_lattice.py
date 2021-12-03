b3acbd14cffb108a2de8293f10a7a80d5c951985
yuzhou42 <yu.nico.zhou@gmail.com>
2021-12-03 11:21:42 -0600

    convert robot psi index from absolute to relative due to different resolution of heading for robot and wheelchair

--- utils_/state_lattice.py
+++ utils_/state_lattice.py
@@ -9,21 +9,23 @@ class StateLattice:
     """
     def __init__(self, sys):
         """Init with system and optimization parameters 
-        # node: (x, y, psi_w, psi_r)
+        # node: (x, y, psi_w, psi_r), notice that psi_w is global index, while psi_r is relative index compare to psi_w, cos they have different resolution
         # edge: computed by solving 2 points boundary problem for 0 and pi/4 directions
         # psi idx:                          0,          1,        2,        3,   4,        5       , 6       7
         # data: rotated SL in 8 direction: [-np.pi*3/4, -np.pi/2, -np.pi/4, 0, np.pi/4, np.pi/2, np.pi*3/4, np.pi]
-
+        # delta_psi(np.pi/6), differece btw robot and wheelchair
         Args:
             sys ([System]): system and optimization parameters
         """
         self.r = sys.sets['res']
         self.sys = sys
-        self.delta_psi = np.pi/4
+        self.delta_psi = np.pi/6
         self.data = {}
         self.psi_set = np.linspace(-np.pi*3/4, np.pi, 8)
-        self.x_curs_axis_idx = [(0,0,3,2),(0,0,3,3),(0,0,3,4)]
-        self.x_curs_diag_idx = [(0,0,4,3),(0,0,4,4),(0,0,4,5)]
+        self.psi_r_set = np.linspace(-self.delta_psi, self.delta_psi, 3)
+
+        self.x_curs_axis_idx = [(0,0,3,-1),(0,0,3,0),(0,0,3,1)]
+        self.x_curs_diag_idx = [(0,0,4,-1),(0,0,4,0),(0,0,4,1)]
         self.cur_idx = [self.x_curs_axis_idx, self.x_curs_diag_idx]
         
         self.axis_idx = [[0, 0, 2], [0, 0, 4], 
@@ -36,6 +38,7 @@ class StateLattice:
                          [1, 1, 4], 
                          [2, 2, 5], [2, 1, 3], [2, 1, 4], [2, 1, 5], [2, 0, 2], [2, 0, 3], [1, 0, 2]]
         self.tgt_idx = [self.axis_idx, self.diag_idx]
+        self.cost_weight = self.sys.sets['cost_weight']
 
         # actural delta pose
         # sl_axis = np.array([[2*r, 2*r, 1/4*np.pi], [2*r, r, 0], [2*r, r,  1/4*np.pi], [2*r, 0, 1/4*np.pi], 
@@ -58,7 +61,7 @@ class StateLattice:
             tgts, states_w, ctrls_w, states_r, costs = [], [], [], [], []
             for x in self.tgt_idx[sl_type]:
                 for i in np.linspace(-1,1,3):
-                    x_tgt_idx = x + [x[2]+int(i)]
+                    x_tgt_idx = x + [int(i)]
                     self.update_umin(x) # handle turning case, as the u is constrained to positive for other cases
                     opt = TrajOpt(self.sys)
                     success = opt.step(stateCurrent = self.idx2pos(x_cur_idx), stateTarget = self.idx2pos(x_tgt_idx), interpolation=False, initMethod = 'linear')
@@ -113,7 +116,7 @@ class StateLattice:
             psi_idx_inc -= sl_type
             for x_cur_idx in self.cur_idx[sl_type]:
                 psi_w_n = (x_cur_idx[2] + psi_idx_inc) % 8
-                psi_r_n = (x_cur_idx[3] + psi_idx_inc) % 8
+                psi_r_n = x_cur_idx[3]
                 x_cur_new = (0,0,psi_w_n,psi_r_n)
                 # print(f"x_cur_idx: {x_cur_idx}")
                 # print(f"x_cur_new: {x_cur_new}")
@@ -170,8 +173,9 @@ class StateLattice:
         """
         state = np.array(state)
         xy = np.round(self.trans_xy(state[:, 0:2], R),0) # not sure why only round works
-        psi = (state[:, 2:] + psi_idx_inc) % 8
-        return np.concatenate((xy,psi), axis = 1).astype(int)
+        psi = np.reshape((state[:, 2] + psi_idx_inc) % 8, (-1,1))
+        psi_r = np.reshape(state[:, 3], (-1,1))
+        return np.concatenate((xy,psi, psi_r), axis = 1).astype(int)
 
     def trans_xy(self, xy, R):
         """translate xy coordinates
@@ -196,7 +200,7 @@ class StateLattice:
             [ndarray]: transformed psi
         """
         # return (np.asarray(psi) + psi_idx_inc) % 8
-        delta_psi = -psi_idx_inc * self.delta_psi
+        delta_psi = -psi_idx_inc * np.pi/4
         for i in range(psi.shape[0]):
             for j in range(psi.shape[1]):
                 psi[i,j] = diff_angle(psi[i,j], delta_psi)
@@ -212,7 +216,7 @@ class StateLattice:
             np.ndarray: node pose
         """
         # print(f"{type(idx)}: {idx}")
-        return np.array([idx[0]*self.r, idx[1]*self.r, self.psi_set[idx[2]], self.psi_set[idx[3]]])
+        return np.array([idx[0]*self.r, idx[1]*self.r, self.psi_set[idx[2]],  self.psi_set[idx[2]] + idx[3] * self.delta_psi])
     
     def M_R(self, psi):
         """compute rotation matrix for SL
@@ -275,7 +279,7 @@ class StateLattice:
             if plot_idx:
                 axs.text(data['states_w'][i][data['states_w'][i].shape[0]//2,0] + 0.1*(i%3 - 1 ), data['states_w'][i][data['states_w'][i].shape[0]//2,1], f"{i},",  fontsize = 10)#bbox=dict(fill=False, edgecolor='red', linewidth=2)
             if plot_cost:
-                axs.text(data['states_w'][i][data['states_w'][i].shape[0]//2,0], data['states_w'][i][data['states_w'][i].shape[0]//2,1]+0.05*(r_orien_idx-1), "{:.2f}".format(data['costs'][i]))#bbox=dict(fill=False, edgecolor='red', linewidth=2)
+                axs.text(data['states_w'][i][data['states_w'][i].shape[0]//2,0], data['states_w'][i][data['states_w'][i].shape[0]//2,1]+0.05*(r_orien_idx-1), "{:.2f}".format(data['costs'][i][0]*self.cost_weight[0] + data['costs'][i][1]*self.cost_weight[1] + data['costs'][i][2]*self.cost_weight[2]))#bbox=dict(fill=False, edgecolor='red', linewidth=2)
             if plot_dir:
                 # if np.tan(data['states_r'][i].obs[-1,2])<5 and np.tan(data['states_r'][i][-1,2])>-5:
                     # axs.arrow(data['states_r'][i][-1,0], data['states_r'][i][-1,1], 0.1, np.tan(data['states_r'][i][-1,2])*0.1, head_width=0.05, head_length=0.05 , fc='yellow', ec='grey') 
