489fbefbfd19909f9bfcff12fba0a24c31f3843d
kmk7733 <54877824+kmk7733@users.noreply.github.com>
2021-12-07 16:32:36 -0600

    Average RRT result

--- evaluation_manager.py
+++ evaluation_manager.py
@@ -108,11 +108,21 @@ def main():
     total_settings = []
     for p_name in eval_settings["planners"]:
         res[p_name] = {}
-        for test in eval_settings["tests"]:
-            world_fn = test["world_fn"]
-            res[p_name][world_fn] = {}
-            for g in test["goals"]:
-                total_settings.append((p_name, world_fn, g))
+        num_itr = 10
+        if p_name == "rrt":
+            for i in range(num_itr):
+                for test in eval_settings["tests"]:
+                    world_fn = test["world_fn"]
+                    res[p_name][world_fn] = {}
+                    for g in test["goals"]:
+                        total_settings.append((p_name, world_fn, g))
+        else:
+            for test in eval_settings["tests"]:
+                world_fn = test["world_fn"]
+                res[p_name][world_fn] = {}
+                for g in test["goals"]:
+                    total_settings.append((p_name, world_fn, g))
+
     for setting in tqdm(total_settings):
         print("Working on setting: ")
         print(setting)
@@ -121,7 +131,38 @@ def main():
         goal[2] = np.radians(goal[2])
         m = EvaluationManager(p_name, world_fn, goal, args["c"])
         # Eventually dump to JSON, all keys need to be strs
-        res[p_name][world_fn][str(g)] = m.run_eval()
+
+        if p_name == "rrt":
+            result = m.run_eval()
+
+            if result["success"] == True:
+                result["success"] = 1
+
+            elif result["success"] == False:
+                result["success"] = 0
+
+            if res[p_name][world_fn] == {}:
+                res[p_name][world_fn][str(g)] = result
+            
+            else:
+                if result["success"] == 1:
+                    for metric in result.keys():
+                        res[p_name][world_fn][str(g)][metric] += result[metric]
+
+        else:
+            res[p_name][world_fn][str(g)] = m.run_eval()
+    
+    for world in res["rrt"]:
+        for goal in res["rrt"][world]:
+            success_num = res["rrt"][world][goal]["success"]
+            print(success_num)
+            if success_num != 0:
+                for metric in res["rrt"][world][goal]:
+                    if metric == "success":
+                        res["rrt"][world][goal][metric] = res["rrt"][world][goal][metric]/num_itr
+                    else: 
+                        res["rrt"][world][goal][metric] = res["rrt"][world][goal][metric]/success_num
+
     with open(args["o"], "w") as f:
         json.dump(res, f, indent="\t")
 
